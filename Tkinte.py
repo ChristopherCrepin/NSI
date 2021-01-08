eb00d89c8c999c8b4542cac71034294ae2385413
Christopher CREPIN <ccrepin@stcharles-stecroix.org>
2021-01-08 14:32:58 +0100

    Update Tkinte.py

--- Tkinte.py
+++ Tkinte.py
@@ -3,6 +3,8 @@ from tkinter import *
 fenetre = Tk()
 label = Label(fenetre, text="Hello World!")
 label.pack()
+
 Bouton_sortie = Button(fenetre, text="PTDR T KI", command=fenetre.quit)
 Bouton_sortie.pack()
+
 fenetre.mainloop()
