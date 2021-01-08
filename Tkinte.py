3dec04cbea4f942dde98394ab53c4d95e7473256
Christopher CREPIN <ccrepin@stcharles-stecroix.org>
2021-01-08 14:46:06 +0100

    Update Tkinte.py

--- Tkinte.py
+++ Tkinte.py
@@ -1,10 +1,15 @@
 from tkinter import *
+from tkinter import Canvas
+import random
 
 fenetre = Tk()
 label = Label(fenetre, text="Hello World!")
 label.pack()
-
+canvas=Canvas()
+for i in range(1000000000000) :
+    canvas.create_line([random.randint(0,200) for i in range(4)])
+    canvas.pack()
 Bouton_sortie = Button(fenetre, text="PTDR T KI", command=fenetre.quit)
 Bouton_sortie.pack()
-
+#fenetre.geometry("400x400+300+300")
 fenetre.mainloop()
