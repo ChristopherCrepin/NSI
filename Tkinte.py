17e54cf2b9340f499778b6f0c5212eb0f7ae34b4
Christopher CREPIN <ccrepin@stcharles-stecroix.org>
2021-01-08 15:21:37 +0100

    Update Tkinte.py

--- Tkinte.py
+++ Tkinte.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from tkinter import Canvas
 import random
-
+def coul_alea() :
+    COLORS = ['snow', 'blue', 'red', 'green','orange','yellow','pink']
+    return random.choice(COLORS)
 fenetre = Tk()
 label = Label(fenetre, text="Hello World!")
 label.pack()
 canvas=Canvas()
-for i in range(1000000000000) :
-    canvas.create_line([random.randint(0,200) for i in range(4)])
+for i in range(1000000) :
+    canvas.create_line(random.randint(0,500),random.randint(0,500),random.randint(0,500),random.randint(0,500), fill=coul_alea())
     canvas.pack()
 Bouton_sortie = Button(fenetre, text="PTDR T KI", command=fenetre.quit)
 Bouton_sortie.pack()
