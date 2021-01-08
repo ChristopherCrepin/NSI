abfb7a4ff5edcd30f53ae825a504ab38e9514bad
Christopher CREPIN <ccrepin@stcharles-stecroix.org>
2021-01-08 15:42:08 +0100

    final project

--- Tkinte.py
+++ Tkinte.py
@@ -1,17 +1,37 @@
 from tkinter import *
 from tkinter import Canvas
 import random
+
+fenetre = None
+
 def coul_alea() :
     COLORS = ['snow', 'blue', 'red', 'green','orange','yellow','pink']
     return random.choice(COLORS)
-fenetre = Tk()
-label = Label(fenetre, text="Hello World!")
-label.pack()
-canvas=Canvas()
-for i in range(1000000) :
-    canvas.create_line(random.randint(0,500),random.randint(0,500),random.randint(0,500),random.randint(0,500), fill=coul_alea())
-    canvas.pack()
-Bouton_sortie = Button(fenetre, text="PTDR T KI", command=fenetre.quit)
-Bouton_sortie.pack()
-#fenetre.geometry("400x400+300+300")
-fenetre.mainloop()
+
+def sameplayerplayagain() :
+    global fenetre
+    if fenetre!=None:
+        windows_close()
+    fenetre = Tk()
+    label = Label(fenetre, text="Surement une oeuvre d'art de picasso !")
+    label.pack()
+    canvas=Canvas()
+    for i in range(10000) :
+        canvas.create_line(random.randint(0,500),random.randint(0,500),random.randint(0,500),random.randint(0,500), fill=coul_alea())
+        canvas.pack()
+    Bouton_jaime = Button(fenetre, text="J'aime", command=sameplayerplayagain)
+    Bouton_jaime.pack()
+    Bouton_sortie = Button(fenetre, text="pas cool mec", command=windows_close)
+    Bouton_sortie.pack()
+    position = "400x400+" + str(random.randint(100,800)) + "+" + str(random.randint(100,800))
+    fenetre.geometry(position)
+    fenetre.mainloop()
+
+
+def windows_close() :
+    global fenetre
+    fenetre.destroy()
+    fenetre = None
+
+
+sameplayerplayagain()
